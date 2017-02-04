70daa575c66fcf35a388545301c2690ca4ffb27c
J <an73620@ens.etsmtl.ca>
2017-02-04 16:48:03 -0500

    Changement partielle des commentaire

--- TP1/src/UtilitaireGrilleGui.java
+++ TP1/src/UtilitaireGrilleGui.java
@@ -1,9 +1,21 @@
-import java.awt.Color;
+/*
+ * Auteur: Jonathan Flores
+ * 		   Dennis Flores
+ * Version:
+ * But:
+ * 
+ */
 
+import java.awt.Color;
 
 public class UtilitaireGrilleGui {
 	
-	
+	/********
+	 * 
+	 * @param grille
+	 * @param nombre
+	 * @return
+	 */
 	public static int [][] obtenirGrille(int[][] grille,String nombre){
 		
 		for(int i=0; i < grille.length; i++){
@@ -18,6 +30,11 @@ public class UtilitaireGrilleGui {
 		return grille;
 	}
 	
+	/**
+	 * 
+	 * @param gui
+	 * @param grille
+	 */
 	public static void afficherGrilleGui(GrilleGui gui,int[][] grille){
 		
 		for(int i=0; i < grille.length; i++){
@@ -32,14 +49,25 @@ public class UtilitaireGrilleGui {
 		
 	}
 	
-	
+	/**
+	 * 
+	 * @param gui
+	 * @param tabSolu
+	 * @param combinaison
+	 * @param x
+	 */
 	public static void afficherSolution(GrilleGui gui, Solution[] tabSolu, String combinaison, boolean x){
 		
 		
 		
 	}
 	
-	
+	/**
+	 * 
+	 * @param grille
+	 * @param nombre
+	 * @return
+	 */
 	public static Solution [] obtenirTabSolutions(int[][] grille, String nombre ){
 		
 		Solution [] tabSolution = new Solution[10];
@@ -49,24 +77,42 @@ public class UtilitaireGrilleGui {
 		
 	}
 	
+	/**
+	 * 
+	 * @param gui
+	 * @param depart
+	 * @param posFin
+	 * @param couleur
+	 */
 	public static void changerCouleurEntre2Coord(GrilleGui gui, Coord depart,Coord posFin, Color couleur){
 		
 		
 	}
 	
+	/**
+	 * 
+	 * @param gui
+	 * @param grille
+	 * @param tabSolution
+	 * @param nombre
+	 * @param depart
+	 * @param Fin
+	 * @return
+	 */
 	public static boolean evaluerSelection(GrilleGui gui, int[][] grille,Solution[] tabSolution, String nombre, Coord depart, Coord Fin){
 		boolean x=true;
 		
 		return x;
 	}
 	
+	/**
+	 * 
+	 * @param gui
+	 */
 	public static void reinitialiserCouleur(GrilleGui gui){
 		
 		
 	}
-	
-	
-	
 	
 	 /**
      * Effectue une pause en mode multi-tâches.
